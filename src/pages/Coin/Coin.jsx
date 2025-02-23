177a131134a758da89f96bd8147e4d200d7c2656
Maddy Ali <maddyoali@gmail.com>
2025-02-22 21:12:28 -0600

    Add additional coin info

--- src/pages/Coin/Coin.jsx
+++ src/pages/Coin/Coin.jsx
@@ -24,13 +24,40 @@ const Coin = () => {
   if (coinData) {
     return (
       <div className="coin-container">
-        <div className="coin-info">
+        <div className="coin-name">
           <img src={coinData.image.large} alt="" />
           <p>
             {coinData.name}
             <br />
             {coinData.symbol.toUpperCase()}
           </p>
+        </div>
+        <div className="coin-info">
+          <ul>
+            <li>
+              Current Price: {currency.symbol}
+              {coinData.market_data.current_price[
+                currency.name
+              ].toLocaleString()}
+            </li>
+            <li>
+              24h Low/High: {""}
+              {currency.symbol}
+              {coinData.market_data.high_24h[
+                currency.name
+              ].toLocaleString()}{" "}
+              {"- "}
+              {currency.symbol}
+              {coinData.market_data.low_24h[currency.name].toLocaleString()}
+            </li>
+            <li>Market Rank: {coinData.market_cap_rank}</li>
+            <li>
+              Market Cap: {currency.symbol}
+              {coinData.market_data.market_cap[currency.name].toLocaleString()}
+            </li>
+          </ul>
+        </div>
+        <div>
           <p className="coin-desc">{coinData.description.en}</p>
         </div>
       </div>
